41d95b7a00918ea8e3607aed3a66db33619d2204
agung.smarts@gmail.com <agung.smarts@gmail.com@e32aa0db-0c9a-ab58-f7ab-f5c71ca808de>
2013-08-21 06:16:03 +0000

    add role user kiai, role table and field id_role in table anggota.

--- trunk/sistren/application/views/menu.php
+++ trunk/sistren/application/views/menu.php
@@ -4,43 +4,48 @@
 			$sub_menu_terpilih = $sub_menu;
 			$role_jabatan = $this->session->userdata('id_jabatan');
 		?>
-    	<ul>					
-			<li <?php if($menu_terpilih == 'home') { echo 'class="current"'; } ?>><a href="<?php echo base_url()?>index.php/home" >Home</a></li>
-        	<li <?php if($menu_terpilih == 'pegawai') { echo 'class="current"'; } ?>><a href="#agenda" class="arrow">Pegawai</a>
-            	<span class="arrow"></span>
-            	<ul id="agenda">
-               		<li <?php if($sub_menu_terpilih == 'daftar_pegawai') { echo 'class="current"'; } ?>><a href="<?php echo base_url() ?>index.php/pegawai/all">Daftar Pegawai Keseluruhan</a></li>
-                   <li <?php if($sub_menu_terpilih == 'daftar_pegawai_sert') { echo 'class="current"'; } ?>><a href="<?php echo base_url() ?>index.php/pegawai">Daftar Pegawai Sertifikasi</a></li>
-                    <?php 		
-						if($role_jabatan == '2' || $role_jabatan == '4'){
-					?>
-					<li <?php if($sub_menu_terpilih == 'tambah_pegawai') { echo 'class="current"'; } ?>><a href="<?php echo base_url() ?>index.php/pegawai/viewTambahPegawai">Tambah Pegawai</a></li>
-                    <?php
-						}
-					?>
-					<li <?php if($sub_menu_terpilih == 'daftar_riwayat') { echo 'class="current"'; } ?>><a href="<?php echo base_url() ?>index.php/pegawai/viewRiwayat">Riwayat Kepegawaian</a></li>
-                </ul>
-            </li>
-            <li <?php if($menu_terpilih == 'user') { echo 'class="current"'; } ?>><a href="#idinventaris" class="inventaris">Data User</a>
-            	<span class="arrow"></span>
-            	<ul id="idinventaris">
-               		<li <?php if($sub_menu_terpilih == 'daftar_user') { echo 'class="current"'; } ?>><a href="<?php echo base_url(); ?>index.php/pengelolaan_inventaris/">Daftar User</a></li>
-                    <?php 		
-						if($role_jabatan == '2' || $role_jabatan == '4'){
-					?>
-					<li <?php if($sub_menu_terpilih == 'tambah_user') { echo 'class="current"'; } ?>><a href="<?php echo base_url(); ?>index.php/pengelolaan_inventaris/add_process">Tambah User</a></li>
-					<?php
-						}
-					?>
-                </ul>
-            </li>
-			<li <?php if($menu_terpilih == 'laporan') { echo 'class="current"'; } ?>><a href="#idlaporan" class="arrow">Laporan</a>
-            	<span class="arrow"></span>
-            	<ul id="idlaporan">
-               		<li <?php if($sub_menu_terpilih == 'laporan_cetak') { echo 'class="current"'; } ?>><a href="<?php echo base_url(); ?>index.php/laporan/laporan_cetak/">Laporan Cetak</a></li>
-                    <li <?php if($sub_menu_terpilih == 'laporan_grafik') { echo 'class="current"'; } ?>><a href="<?php echo base_url(); ?>index.php/laporan">Laporan Grafik</a></li>
-                </ul>
-            </li>
+    	<ul>
+            <?php if($this->session->userdata('id_role') == 1)
+            { ?>
+    			<li <?php if($menu_terpilih == 'home') { echo 'class="current"'; } ?>><a href="<?php echo base_url()?>index.php/home" >Home</a></li>
+            	<li <?php if($menu_terpilih == 'pegawai') { echo 'class="current"'; } ?>><a href="#agenda" class="arrow">Pegawai</a>
+                	<span class="arrow"></span>
+                	<ul id="agenda">
+                   		<li <?php if($sub_menu_terpilih == 'daftar_pegawai') { echo 'class="current"'; } ?>><a href="<?php echo base_url() ?>index.php/pegawai/all">Daftar Pegawai Keseluruhan</a></li>
+                       <li <?php if($sub_menu_terpilih == 'daftar_pegawai_sert') { echo 'class="current"'; } ?>><a href="<?php echo base_url() ?>index.php/pegawai">Daftar Pegawai Sertifikasi</a></li>
+                        <?php 		
+    						if($role_jabatan == '2' || $role_jabatan == '4'){
+    					?>
+    					<li <?php if($sub_menu_terpilih == 'tambah_pegawai') { echo 'class="current"'; } ?>><a href="<?php echo base_url() ?>index.php/pegawai/viewTambahPegawai">Tambah Pegawai</a></li>
+                        <?php
+    						}
+    					?>
+    					<li <?php if($sub_menu_terpilih == 'daftar_riwayat') { echo 'class="current"'; } ?>><a href="<?php echo base_url() ?>index.php/pegawai/viewRiwayat">Riwayat Kepegawaian</a></li>
+                    </ul>
+                </li>
+                <li <?php if($menu_terpilih == 'user') { echo 'class="current"'; } ?>><a href="#idinventaris" class="inventaris">Data User</a>
+                	<span class="arrow"></span>
+                	<ul id="idinventaris">
+                   		<li <?php if($sub_menu_terpilih == 'daftar_user') { echo 'class="current"'; } ?>><a href="<?php echo base_url(); ?>index.php/pengelolaan_inventaris/">Daftar User</a></li>
+                        <?php 		
+    						if($role_jabatan == '2' || $role_jabatan == '4'){
+    					?>
+    					<li <?php if($sub_menu_terpilih == 'tambah_user') { echo 'class="current"'; } ?>><a href="<?php echo base_url(); ?>index.php/pengelolaan_inventaris/add_process">Tambah User</a></li>
+    					<?php
+    						}
+    					?>
+                    </ul>
+                </li>
+            <?php } 
+            else if($this->session->userdata('id_role') == 1 || $this->session->userdata('id_role') == 2) { ?>
+    			<li <?php if($menu_terpilih == 'laporan') { echo 'class="current"'; } ?>><a href="#idlaporan" class="arrow">Laporan</a>
+                	<span class="arrow"></span>
+                	<ul id="idlaporan">
+                   		<li <?php if($sub_menu_terpilih == 'laporan_cetak') { echo 'class="current"'; } ?>><a href="<?php echo base_url(); ?>index.php/laporan/laporan_cetak/">Laporan Cetak</a></li>
+                        <li <?php if($sub_menu_terpilih == 'laporan_grafik') { echo 'class="current"'; } ?>><a href="<?php echo base_url(); ?>index.php/laporan">Laporan Grafik</a></li>
+                    </ul>
+                </li>
+            <?php } ?>
         </ul>
         <a class="togglemenu"></a>
         <br /><br />
